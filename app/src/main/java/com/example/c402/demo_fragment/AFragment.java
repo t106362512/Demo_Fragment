4c9f4e0b064d78070d8c99568fb1dac963ff57d0
C402-01\C402 <t106362512>
2018-07-19 16:29:04 +0800

    20180719

--- app/src/main/java/com/example/c402/demo_fragment/AFragment.java
+++ app/src/main/java/com/example/c402/demo_fragment/AFragment.java
@@ -2,18 +2,25 @@ package com.example.c402.demo_fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AFragment extends Fragment {
+public class AFragment extends Fragment{
 
-
+    TextView cAF;
+    int i;
     public AFragment() {
         // Required empty public constructor
     }
@@ -25,5 +32,21 @@ public class AFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_a, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        cAF = getView().findViewById(R.id.AFT);
+        cAF.setOnClickListener((View v) -> {
+            FragmentTransaction ft = ((MainActivity)getActivity()).getSupportFragmentManager().beginTransaction();
+            ft.setCustomAnimations(R.anim.view_animation_set,R.anim.fade_out_left);
+            ft.replace(R.id.frameA,AFragment.this);
+            ft.commit();
+
+            cAF.setText(""+(i++));
+
+        });
+    }
+
 
 }
--- app/src/main/java/com/example/c402/demo_fragment/MainActivity.java
+++ app/src/main/java/com/example/c402/demo_fragment/MainActivity.java
@@ -4,22 +4,46 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.security.spec.RSAPrivateCrtKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button TestB;
+    AFragment af = new AFragment();
+    BFragment bf = new BFragment();
+
+    FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        AFragment af = new AFragment();
-        BFragment bf = new BFragment();
-
-        FragmentManager fm = getSupportFragmentManager();
+        TestB = findViewById(R.id.button);
+        TestB.setOnClickListener((View v) -> {replaceFragment();});
         FragmentTransaction ft = fm.beginTransaction();
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.setCustomAnimations(R.anim.view_animation_set,R.anim.fade_out_left);
         ft.add(R.id.frameA,af);
         ft.replace(R.id.frameB,bf);
-        ft.commitNow();
+        ft.addToBackStack(null);
+        ft.commit();
 
     }
+
+
+    public void replaceFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.frameA,af)
+                .replace(R.id.frameB,bf)
+                .addToBackStack(null);
+                ft.commit();
+    }
+
 }
